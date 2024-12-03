b66c2b8e5fefde5ec104c3bd05fd1599c96c2d17
Sarah <151523107+vw09@users.noreply.github.com>
2024-12-03 14:42:15 +0100

    Opstart basic screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,9 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+ <Stack.Screen name="index" options={{ title: 'Home' }} />
+ <Stack.Screen name="library" options={{ title: 'Library' }} /> 
+ <Stack.Screen name="diary" options={{ title: 'Diary' }} /> 
+  </Stack>
   );
 }
